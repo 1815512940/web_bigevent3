d635d7609903cf50eb05d382f7fc1c22aa3f9830
itwwx <1815512940@qq.com>
2020-11-26 20:01:23 +0800

    主页功能完成

--- assets/js/baseApl.js
+++ assets/js/baseApl.js
@@ -1,4 +1,23 @@
 var baseAPL = 'http://ajax.frontend.itheima.net'
+
+// 1. 所欲ajax都要进行的配置
+// 2.大部分ajax都要进行的操作
+// 3.有规律的ajax进行的特有操作
 $.ajaxPrefilter(function (params) {
     params.url = baseAPL + params.url
+
+    if (params.url.indexOf('/my/') !== -1) {
+        params.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 拦截所有响应 ，判断身份认证信息
+    params.complete = function (res) {
+        if (res.responseJSON.status == 1 && res.responseJSON.message == '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
+
 })
---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+$(function () {
+    getUserInfo()
+})
+
+function getUserInfo() {
+    $.ajax({
+        url: '/my/userinfo',
+        success: function (res) {
+            console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message)
+            }
+            renderAvater(res.data)
+        }
+        
+    })
+}
+
+function renderAvater(user) {
+    var name = user.nickname || user.username
+    $('#welcome').html(`欢迎  ${name}`)
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').show().prop('scr', user.user_pic)
+        $('.text-avater').hide()
+    } else {
+        var text = name[0].toUpperCase()
+        $('.text-avater').html(text).show()
+        $('.layui-nav-img').hide()
+    }
+}
+
+$('#btnLogout').on('click', function () {
+    layer.confirm('是否确定退出?', {icon: 3, title:'提示'}, function(index){
+        //do something
+        localStorage.removeItem('token')
+        location.href = '/login.html'
+        layer.close(index);
+      })
+})
